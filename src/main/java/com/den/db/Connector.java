3fd647312102cbd55cc0b6ac0ba4cdaa8ed8faff
DenisGorbach <denisgorbachit@gmail.com>
2016-10-16 17:20:14 +0300

    Load project.

---
+++ src/main/java/com/den/db/Connector.java
@@ -0,0 +1,90 @@
+package com.den.db;
+
+
+import java.sql.*;
+
+public class Connector {
+
+    private Connection connection = null;
+    private Statement statement;
+    private ResultSet resultSet;
+
+    public void connect(){
+        try {
+            Class.forName("org.sqlite.JDBC");
+            connection = DriverManager.getConnection("jdbc:sqlite:db.db");
+            statement = connection.createStatement();
+            statement.setQueryTimeout(30);  // set timeout to 30 sec.
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public Statement getStatement(){
+        return this.statement;
+    }
+
+    public ResultSet getResultSet(String query){
+        try {
+            resultSet = getStatement().executeQuery(query);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return resultSet;
+    }
+
+    public void executeQuery(String query){
+        try {
+            getStatement().executeUpdate(query);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void closeConnection(){
+        if (connection!=null) try {
+            connection.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) throws ClassNotFoundException
+    {
+            Connector connector = new Connector();
+            connector.connect();
+
+            connector.executeQuery("insert into crawler values(null, '{\n" +
+                    "\"group_id\": 2,\n" +
+                    "\"social_network\": \"vk\",\n" +
+                    "\"response\":[{\n" +
+                    "\"id\":210700286,\n" +
+                    "\"first_name\":\"Lindsey\",\n" +
+                    "\"last_name\":\"Stirling\",\n" +
+                    "\"company\":\"DoIt\"\n" +
+                    "},\n" +
+                    "{\n" +
+                    "\"id\":210700286,\n" +
+                    "\"first_name\":\"Lindsey\",\n" +
+                    "\"last_name\":\"Stirling\",\n" +
+                    "\"company\":\"Sigma\"\n" +
+                    "}\n" +
+                    "]\n" +
+                    "}')");
+            ResultSet rs = connector.getResultSet("select * from crawler");
+        try {
+            while(rs.next())
+            {
+                // read the result set
+                System.out.println("json = " + rs.getString("result"));
+                System.out.println("id = " + rs.getInt("id"));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+            connector.closeConnection();
+    }
+}
---
+++ src/main/java/com/den/model/User.java
@@ -0,0 +1,55 @@
+package com.den.model;
+
+
+
+public class User {
+    private int id;
+    private String first_name;
+    private String last_name;
+    private String company;
+
+//    public User(int id, String firstName, String last_name, String company) {
+//        this.id = id;
+//        this.first_name = firstName;
+//        this.last_name = last_name;
+//        this.company = company;
+//    }
+
+    public User(){
+
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirst_name() {
+        return first_name;
+    }
+
+    public void setFirst_name(String first_name) {
+        this.first_name = first_name;
+    }
+
+    public String getLast_name() {
+        return last_name;
+    }
+
+    public void setLast_name(String last_name) {
+        this.last_name = last_name;
+    }
+
+    public String getCompany() {
+        return company;
+    }
+
+    public void setCompany(String company) {
+        this.company = company;
+    }
+}
+
+
